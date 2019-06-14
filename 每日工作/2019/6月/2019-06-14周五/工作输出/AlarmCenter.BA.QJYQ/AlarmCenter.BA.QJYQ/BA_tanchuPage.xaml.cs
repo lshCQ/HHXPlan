8d3704ffb538c83472ce091ff7dcee3d38aa7fa9
CQGW_HHX <1978545344@qq.com>
2019-06-14 17:29:01 +0800

    2019-06-14 HHX工作输出，WPF页面

---
+++ 每日工作/2019/6月/2019-06-14周五/工作输出/AlarmCenter.BA.QJYQ/AlarmCenter.BA.QJYQ/BA_tanchuPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using AlarmCenter.GWWpfCustomControlLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AlarmCenter.BA.QJYQ
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class BA_tanchuPage : GWPageContentControl
+    {
+        public BA_tanchuPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
